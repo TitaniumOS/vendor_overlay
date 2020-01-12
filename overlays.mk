61913fbf1ace1c45c8e2059f802764bce431db0c
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-01-11 21:57:46 -0500

    Overlay: Add Nature primary
    Change-Id: I6da31f12563d4d7ed1c3903f69d878d34bb07c2e

--- overlays.mk
+++ overlays.mk
@@ -4,7 +4,8 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common/
 # Extra packages
 PRODUCT_PACKAGES += \
     PrimaryColorBlackOverlay \
-    PrimaryColorOceanOverlay
+    PrimaryColorOceanOverlay \
+    PrimaryColorAOSiPNatureOverlay
 
 # Prebuilts
 $(call inherit-product-if-exists, vendor/prebuilts/packages.mk)
