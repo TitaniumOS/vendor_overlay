2d9503c7261d178edb1f6b765014b3c64d0d57ba
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-01-11 21:56:54 -0500

    Overlay: Add Ocean primary
    Change-Id: I61f17d7475feab26eddda3b928c423904bc934a4

--- overlays.mk
+++ overlays.mk
@@ -3,7 +3,8 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common/
 
 # Extra packages
 PRODUCT_PACKAGES += \
-    PrimaryColorBlackOverlay
+    PrimaryColorBlackOverlay \
+    PrimaryColorOceanOverlay
 
 # Prebuilts
 $(call inherit-product-if-exists, vendor/prebuilts/packages.mk)
